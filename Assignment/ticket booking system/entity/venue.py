eef03258404f9652d3b6c83f9bbe372cec53cd21
kaveeya-dharsh <kaveeyadharshni@gmail.com>
2025-04-23 13:03:00 +0530

    Delete Assignment/ticket booking system directory

--- Assignment/ticket booking system/entity/venue.py
+++
@@ -1,7 +0,0 @@
-class Venue:
-    def __init__(self, venue_name: str, address: str):
-        self.venue_name = venue_name
-        self.address = address
-
-    def display_venue_details(self):
-        print(f"Venue: {self.venue_name}\nAddress: {self.address}")
